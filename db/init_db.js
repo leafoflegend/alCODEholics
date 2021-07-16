bd6fbd4d78d2b591dde679a87e2e7af75362d828
Maxwell Montegary <maxwellmontegary@gmail.com>
2021-07-16 16:11:36 -0700

    Merge chris branch

--- db/init_db.js
+++ db/init_db.js
@@ -1,23 +1,11 @@
 // code to build and initialize DB goes here
-<<<<<<< HEAD
-const {
-  client
-  // other db methods 
-} = require('./index');
-=======
 const { client, createAlcohol } = require("./index");
->>>>>>> 1cdd146b11e87071783bf02c49178ab59f14fe56
 
 async function buildTables() {
   try {
     client.connect();
 
     // drop tables in correct order
-<<<<<<< HEAD
-
-    // build tables in correct order
-
-=======
     console.log("Starting to DROP TABLES...");
     await client.query(`
     DROP TABLE IF EXISTS alcohols
@@ -34,7 +22,6 @@ async function buildTables() {
         )`
     );
     console.log("Successfully CREATED TABLES!");
->>>>>>> 1cdd146b11e87071783bf02c49178ab59f14fe56
   } catch (error) {
     throw error;
   }
@@ -42,9 +29,6 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
-<<<<<<< HEAD
-    // create useful starting data
-=======
     const alcoholsToCreate = [
       {
         type: "Rum",
@@ -65,7 +49,6 @@ async function populateInitialData() {
     console.log("Alcohols Created");
     console.log("Alcohols Created: ", alcohols);
     console.log("Finished creating alcohols!");
->>>>>>> 1cdd146b11e87071783bf02c49178ab59f14fe56
   } catch (error) {
     throw error;
   }
@@ -74,8 +57,4 @@ async function populateInitialData() {
 buildTables()
   .then(populateInitialData)
   .catch(console.error)
-<<<<<<< HEAD
   .finally(() => client.end());
-=======
-  .finally(() => client.end());
->>>>>>> 1cdd146b11e87071783bf02c49178ab59f14fe56
