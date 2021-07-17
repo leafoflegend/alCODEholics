33d4ec50084c822fc07a2c7ef3dcbddf74fd355e
Jordan Hawley <mjthriller1993@gmail.com>
2021-07-17 12:11:51 -0700

    Added a GET API ROUTE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const apiRouter = require('express').Router();
 
+const { getUser } = require('../db/index')
+
 apiRouter.get("/", (req, res, next) => {
   res.send({
     message: "API is under construction!"
   });
 });
+
+apiRouter.get("/users", async (req, res, next) => {
+
+  try {
+    const user = await getUser()
+
+  res.send({
+    user: user
+  })
+  } catch (error) {
+    throw error
+  }
+  
+})
 
 module.exports = apiRouter;
