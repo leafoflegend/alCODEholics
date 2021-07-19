df94e2545158b0ed19473365e490da0f91bb695a
Jordan Hawley <mjthriller1993@gmail.com>
2021-07-19 12:10:43 -0700

    Added two more columns to the alcohols table, also remade the Front End to display alcohol as cards

--- db/alcohols_db.js
+++ db/alcohols_db.js
@@ -1,14 +1,14 @@
 const client = require('./client')
 
-const createAlcohol = async ({ type, name, inStock }) => {
+const createAlcohol = async ({ type, name, price, description, inStock }) => {
   try {
     const { rows } = await client.query(
       `
-          INSERT INTO alcohols(type, name, "inStock")
-          VALUES($1, $2, $3)
+          INSERT INTO alcohols(type, name, price, description, "inStock")
+          VALUES($1, $2, $3, $4, $5)
           RETURNING *
           `,
-      [type, name, inStock]
+      [type, name, price, description, inStock]
     );
 
     return rows;
@@ -20,7 +20,7 @@ const createAlcohol = async ({ type, name, inStock }) => {
 const getAllAlcohol = async () => {
   try {
     const { rows } = await client.query(`
-  SELECT type, name, "inStock"
+  SELECT type, name, price, description, "inStock"
   FROM alcohols
   `);
 
