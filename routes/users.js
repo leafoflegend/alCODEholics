f7d5eebdd745f7c6c36545051e0fb609a264b226
Chris Wong <cw12355@gmail.com>
2021-07-20 15:27:46 -0700

    fixing register users

--- routes/users.js
+++ routes/users.js
@@ -23,8 +23,8 @@ usersRouter.get('/:userId', async (req, res, next) => {
 })
 
 usersRouter.post('/register', async (req, res, next) => {
-    const { username, password } = req.body;
-    const user = await registerUser(username, password);
+    const { username, password, isAdmin } = req.body;
+    const user = await registerUser({username, password, isAdmin});
 
     res.send({
         user: user,
